39f6ab55a4cf1db41ef8c6bcd74d461127d9d906
pradhuman1 <pradhumansinghbaid@gmail.com>
2021-04-27 13:14:13 +0530

    delete user response message updated

--- functionalities/user/removeUser.js
+++ functionalities/user/removeUser.js
@@ -6,18 +6,20 @@ const { Client } = require('../../utils/db.js');
 exports.removeUser = async function (req, res) {
     const user_id = req.body.user_id;
 
-
     const client = await Client();
 
     await client
         .query(`DELETE FROM credentials WHERE user_id=$1`, [user_id])
         .then(resData => {
-            res.status(200).send(` '${user_id}' Removed`);
+            if(resData.rowCount)
+            res.status(200).send({msg:"User removed"});
+            else
+            res.status(400).send({msg:"User not exist"});
 
         })
         .catch(err => {
             console.error(err);
-            res.status(400).send(`User Not found`);
+            res.status(400).send({msg:"User not found"});
 
         });
     await client.release();
